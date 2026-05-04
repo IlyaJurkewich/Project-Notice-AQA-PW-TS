f28efcf428d0c4c66220528a5a7e7b750a802aa9
ilya yurkevich <ilya.yurkevich.pl@gmail.com>
2026-05-05 00:12:54 +0200

    add headless fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
     trace: 'on-first-retry',
     screenshot: 'only-on-failure',
     video: 'on-first-retry',
-    headless: false,
+    headless: process.env.CI ? true : false,
   },
   projects: [
     {
